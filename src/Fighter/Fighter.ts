56c17a8e64a431bf49578a6d38593f1bd7635555
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-09-22 19:49:37 -0300

    feat: criada interface Fighter

---
+++ src/Fighter/Fighter.ts
@@ -0,0 +1,12 @@
+import Energy from '../Energy';
+
+export default interface IFighter {
+  lifePoints: number;
+  strength: number;
+  defense: number;
+  energy?: Energy;
+  attack: (enemy: IFighter) => void;
+  special?: (enemy: IFighter) => void;
+  levelUp: () => void;
+  receiveDamage: (attackPoints: number) => number;
+}
